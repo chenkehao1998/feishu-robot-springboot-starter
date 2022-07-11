4539d54435f874061b9e811f484284399d45ba20
kehao <754772661@qq.com>
2022-07-11 20:45:06 +0800

    当配置了Url才能加载配置类，否则不能加载配置

--- src/main/java/cn/kehao/feishu/robot/config/FeiShuRobotConfiguration.java
+++ src/main/java/cn/kehao/feishu/robot/config/FeiShuRobotConfiguration.java
@@ -1,17 +1,17 @@
 package cn.kehao.feishu.robot.config;
 
 import cn.kehao.feishu.robot.pojo.FeiShuRobotProperties;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@ConditionalOnProperty(prefix = "kehao.feishu.robot",name = "webhookUrl")
+@ConditionalOnExpression("'${kehao.feishu.robot.webhook-url}'.startsWith('http')")
 @EnableAspectJAutoProxy
 @EnableConfigurationProperties({FeiShuRobotProperties.class})
 @ComponentScan(basePackages = "cn.kehao.feishu.robot")
+@Configuration
 public class FeiShuRobotConfiguration {
 }
